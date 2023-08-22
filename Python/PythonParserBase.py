7842cad9c1d9bd4a3e66d318e3e7396eddeed762
Robert Einhorn <robert.einhorn.hu@gmail.com>
2023-08-22 13:24:34 +0200

    some update

--- Python/PythonParserBase.py
+++ Python/PythonParserBase.py
@@ -1,8 +1,7 @@
-from antlr4 import Parser, Token, InputStream
+from antlr4 import InputStream, Parser, Token
 from typing import TextIO
 from IndentationErrorListener import IndentationErrorListener
 import sys
-
 
 class PythonParserBase(Parser):
     def __init__(self, input: InputStream, output: TextIO = sys.stdout):
@@ -10,6 +9,40 @@ class PythonParserBase(Parser):
         self._isMatchedEOF = False
         #self.removeErrorListeners()
         self.addErrorListener(IndentationErrorListener())
+
+    def isCurrentTokenType1(self, type1: int) -> bool:
+        currentTokenType = self.getCurrentToken().type
+        return currentTokenType == type1
+
+    def isCurrentTokenType2(self, type1: int, type2: int) -> bool:
+        currentTokenType = self.getCurrentToken().type
+        return currentTokenType == type1 \
+            or currentTokenType == type2
+
+    def isCurrentTokenType3(self, type1: int, type2: int, type3: int) -> bool:
+        currentTokenType = self.getCurrentToken().type
+        return currentTokenType == type1 \
+            or currentTokenType == type2 \
+            or currentTokenType == type3
+
+    def isnotCurrentTokenType1(self, type1: int) -> bool:
+        return not self.isCurrentTokenType1(type1)
+
+    def isnotCurrentTokenType2(self, type1: int, type2: int) -> bool:
+        return not self.isCurrentTokenType2(type1, type2)
+
+    def isnotCurrentTokenType3(self, type1: int, type2: int, type3: int) -> bool:
+        return not self.isCurrentTokenType3(type1, type2, type3)
+
+    def isTokenTypeSequence(self, type1: int, type2: int) -> bool:
+        return self._input.LA(1) == type1 \
+           and self._input.LA(2) == type2
+
+    def isCurrentTokenText(self, tokenText: str) -> bool:
+        return self.getCurrentToken().text == tokenText
+
+    def isnotCurrentTokenText(self, tokenText: str) -> bool:
+        return not self.isCurrentTokenText(tokenText)
 
     def exitRule(self):
         super().exitRule()
