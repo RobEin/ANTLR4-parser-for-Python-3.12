27d6a077885229554e9c41257f7e5079817fd3e5
Robert Einhorn <robert.einhorn.hu@gmail.com>
2023-09-24 21:50:15 +0200

    C# target

---
+++ target_CSharp/README.md
@@ -0,0 +1,28 @@
+### C# target
+
+#### Command line example:
+- first create a C# project called grun_tokens then copy the two grammar files and test.py to this directory
+```bash
+    dotnet new console -o . -n grun_tokens
+    del program.cs
+    dotnet add package Antlr4.Runtime.Standard --version 4.13.1
+```
+
+```bash
+    copy ..\*.g4
+    copy ..\test.py
+```
+
+```bash
+antlr4 -Dlanguage=CSharp PythonLexer.g4
+antlr4 -Dlanguage=CSharp PythonParser.g4
+dotnet build
+.\bin\Debug\net7.0\grun_tokens.exe test.py
+```
+
+#### Related link:
+[C# target](https://github.com/antlr/antlr4/blob/dev/doc/csharp-target.md)
+
+[Download .NET](https://dotnet.microsoft.com/en-us/download)
+
+[Antlr4 Standard Runtime](https://www.nuget.org/packages/Antlr4.Runtime.Standard/)
---
+++ target_CSharp/grun_tokens.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Antlr4.Runtime;
+
+[assembly: CLSCompliant(false)]
+
+namespace grun_tokens;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        var input = CharStreams.fromPath(args[0]);
+        var lexer = new PythonLexer(input);
+        var tokens = new CommonTokenStream(lexer);
+        var parser = new PythonParser(tokens);
+
+        tokens.Fill();
+        foreach (IToken t in tokens.GetTokens())
+        {
+            Console.WriteLine(GetTokenMetaDataWithRuleName(parser, t));
+        }
+
+        parser.file_input();
+    }
+
+    private static string GetTokenMetaDataWithRuleName(PythonParser parser, IToken token)
+    {
+        String metaData = token.ToString()!;          // original format: [@TokenIndex,startIndex:stopIndex='text',<TokenType>,line:charPositionInLine]
+        int lesserPos = metaData!.LastIndexOf(",<")!;
+        int greaterPos = metaData!.LastIndexOf(">,")!;
+        return metaData.Substring(0, lesserPos + 2) + // modified format: [@TokenIndex,startIndex:stopIndex='text',<TokenType is RuleName>,line:charPositionInLine]
+                parser.Vocabulary.GetSymbolicName(token.Type) +
+                metaData.Substring(greaterPos);
+    }
+}
